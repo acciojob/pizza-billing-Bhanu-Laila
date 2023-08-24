8e344de4bb4b9ecf8419ac8f273d48ee45f6e921
Bhanu-Laila <bhanulaila1998@gmail.com>
2023-08-25 02:46:46 +0530

    code3

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -29,6 +29,8 @@ public class Pizza {
         }
         cheesePrice = 80;
         paperBagPrice = 20;
+        totalPrice = totalPrice + basePrice;
+        bill = bill + "Base Price Of The Pizza: " + basePrice+ "/n";
 
     }
 
